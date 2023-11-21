59bf8f48a5012acc0ebc92a6f9ecfe8e1af57acb
ujinthegrey <v4deus@gmail.com>
2023-11-21 20:13:28 +0400

    add base.js make full the modal from base with button click

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+import { tract001 } from '/src/base.js'
+
 const body = document.getElementById('body')
 const dialog = document.getElementById('dialog')
 const openBtn = document.getElementById('open')
 const closeBtn = document.getElementById('close')
 
+const audio = document.getElementById('audio')
+const author = document.getElementById('author')
+const bible = document.getElementById('bible')
+const date = document.getElementById('date')
+const text = document.getElementById('text')
+const title = document.getElementById('title')
+const number = document.getElementById('number')
+
+
 openBtn.onclick = () => {
+    audio.setAttribute('src', tract001.audio || '')
+    author.textContent = tract001.author || ''
+    author.setAttribute('href', tract001.link || '*')
+    bible.textContent = tract001.bible || ''
+    date.textContent = tract001.date || ''
+    title.textContent = tract001.title || ''
+    number.textContent = tract001.number || ''
+    text.textContent = tract001.text || ''
+
     body.classList.toggle('._locked')
     dialog.showModal()
 }
@@ -12,3 +32,5 @@ closeBtn.onclick = () => {
     dialog.close()
     body.classList.toggle('._locked')
 }
+
+console.log(tract001);
