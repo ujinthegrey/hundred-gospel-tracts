ca67fa403531d0b625054bfb11ccac51ae7fdbf6
ujinthegrey <v4deus@gmail.com>
2023-11-24 18:12:16 +0400

    add tracts array with all data, add parcing links from tracts array

--- app.js
+++ app.js
@@ -1,8 +1,69 @@
-import { tract001 } from '/src/base.js'
+const tracts = [
+    {
+        "audio": '',
+        "author": '',
+        "bible": '',
+        "bibleText": '',
+        "date": 'Самара, 2020',
+        "id": 0,
+        "link": '',
+        "number": '',
+        "text": 'Мир Вам! Мы рады представить проект "100 Евангельских Трактатов", в которым мы хотим возродить искусство Евангельскогго трактата. Что такое Евангельский трактат? Это проповедь или, по-другому, презентация самого главного в христианской вере: Евангелия. Благая весть, по-гречески, Евангелион содержит в себе истины о том, что Бог сотворил этот мир и имеет полное право определять смысл жизни человека. А потому человек несёт ответственность перед Творцом за свои грехи. Евангелие определяет грех не как игривое заступание за черту, нанесённую веткой на песке, а как тяжелейшее оскорбление Личности Бога. Бог же, будучи совершенным, не намерен терпеть, когда Его оскорбляют. Поэтому всех людей ожидает Божий суд, на котором не будет никаких "взвешиваний" добрых и злых дел. Наличие одного греха это достаточное основание для приговора к вечному наказанияю. И у человека нет никаких средств снять с себя вину грехов. Но Бог отдал Своего святого Сына в жертву за грехи на голгофском кресте. Теперь любой человек может оправдаться даром на основании жертвы Христа. Евангелие говорит каждому человеку поверить в Спасителя и обещает вечную жизнь с Ним в новом мире без греха и страданий. Все эти имтины, которые составляют Евангелие, чрезвычайно глубоки. И очень трудно объяснять глубокие вещи доступно. Именно поэтому мы называем Евангельский трактат искусством. Именно поэтому мы привлекли к проекту только обученных и проверенных проповедников Библии. Мы надеемся, что, читая и слушая Евангельские трактаты, Вы не только поймёте суть пути спасения, но и проникнитесь глубиной этих истин. Молимся о спасении Вашей души.',
+        "title": 'О проекте',
+    },
+    {
+        "audio": '/audio/001.mp3',
+        "author": 'Алексей Прокопенко',
+        "bible": 'Псалом 89',
+        "bibleText": '1 Молитва Моисея, человека Божия',
+        "date": 'Самара, 2020',
+        "id": 1,
+        "link": 'https://www.youtube.com/@alprokope',
+        "number": '001',
+        "text": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolorem. Ad temporibus quis unde blanditiis expedita soluta, accusamus tempore ullam dolores! Nisi minima soluta odit ipsam ad repudiandae consectetur deserunt quas sit, nihil saepe corporis esse? Quod perspiciatis nesciunt doloremque, quaerat excepturi provident dignissimos sed? Provident voluptates est blanditiis vel, id a quaerat, odio ipsam dolores, doloribus minus labore. Voluptatem corrupti culpa perferendis amet sint placeat mollitia quos, magnam iste, ratione minima commodi quis? Consequatur dolor, minus id quos eum voluptas corrupti pariatur optio incidunt asperiores quam rerum consequuntur nam quis accusantium non ex ipsum a molestiae labore necessitatibus omnis cupiditate. Esse sapiente provident quo necessitatibus placeat, nesciunt accusantium quaerat aperiam vero hic expedita debitis voluptas illo pariatur vitae. Molestiae, aperiam voluptates ullam culpa odio veritatis eligendi ea vitae repellendus dolorum quis alias, dolores rerum earum ipsum optio voluptatem est nulla nemo voluptas reprehenderit? Pariatur laboriosam tempore rem magni doloribus repellendus nisi quam animi quasi! Dignissimos ab quia facilis, nisi maiores ad quasi obcaecati laboriosam cupiditate architecto, consequatur rem fuga incidunt adipisci provident, fugiat repellat dolore! Repellendus numquam, nihil veniam optio accusamus excepturi ipsum omnis natus doloremque nobis eius, expedita ipsa provident laborum. Itaque sint maxime nostrum sed eius fuga officiis, facere reprehenderit repellendus adipisci, excepturi quasi qui corporis labore nulla odio vitae porro. Officiis qui nisi quia fugit suscipit quibusdam expedita dolor ut. Inventore in, quisquam aliquam culpa perferendis ipsam, doloribus praesentium aperiam voluptatibus corporis natus. Error velit sapiente eum? Modi qui facere officiis velit quaerat amet sed nam voluptas, laborum tempora aliquid dolorem numquam nobis provident quidem culpa voluptates neque porro quae! Repellat odit doloremque, rem sint suscipit neque saepe doloribus assumenda sed natus. Sint quasi excepturi commodi placeat delectus omnis cupiditate incidunt voluptatem natus repellat, illo accusamus? Odio dolor ipsam in ea eveniet unde voluptatibus, officia libero.',
+        "title": 'Временный перед лицом Вечного',
+    },
+    {
+        "audio": '/audio/001.mp3',
+        "author": 'Евгений Кошелев',
+        "bible": 'Евангелие от Иоанна 3',
+        "bibleText": '',
+        "date": 'Самара, 2023',
+        "id": 2,
+        "link": 'https://www.youtube.com/@alprokope',
+        "number": '002',
+        "text": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolorem. Ad temporibus quis unde blanditiis expedita soluta, accusamus tempore ullam dolores! Nisi minima soluta odit ipsam ad repudiandae consectetur deserunt quas sit, nihil saepe corporis esse? Quod perspiciatis nesciunt doloremque, quaerat excepturi provident dignissimos sed? Provident voluptates est blanditiis vel, id a quaerat, odio ipsam dolores, doloribus minus labore. Voluptatem corrupti culpa perferendis amet sint placeat mollitia quos, magnam iste, ratione minima commodi quis? Consequatur dolor, minus id quos eum voluptas corrupti pariatur optio incidunt asperiores quam rerum consequuntur nam quis accusantium non ex ipsum a molestiae labore necessitatibus omnis cupiditate. Esse sapiente provident quo necessitatibus placeat, nesciunt accusantium quaerat aperiam vero hic expedita debitis voluptas illo pariatur vitae. Molestiae, aperiam voluptates ullam culpa odio veritatis eligendi ea vitae repellendus dolorum quis alias, dolores rerum earum ipsum optio voluptatem est nulla nemo voluptas reprehenderit? Pariatur laboriosam tempore rem magni doloribus repellendus nisi quam animi quasi! Dignissimos ab quia facilis, nisi maiores ad quasi obcaecati laboriosam cupiditate architecto, consequatur rem fuga incidunt adipisci provident, fugiat repellat dolore! Repellendus numquam, nihil veniam optio accusamus excepturi ipsum omnis natus doloremque nobis eius, expedita ipsa provident laborum. Itaque sint maxime nostrum sed eius fuga officiis, facere reprehenderit repellendus adipisci, excepturi quasi qui corporis labore nulla odio vitae porro. Officiis qui nisi quia fugit suscipit quibusdam expedita dolor ut. Inventore in, quisquam aliquam culpa perferendis ipsam, doloribus praesentium aperiam voluptatibus corporis natus. Error velit sapiente eum? Modi qui facere officiis velit quaerat amet sed nam voluptas, laborum tempora aliquid dolorem numquam nobis provident quidem culpa voluptates neque porro quae! Repellat odit doloremque, rem sint suscipit neque saepe doloribus assumenda sed natus. Sint quasi excepturi commodi placeat delectus omnis cupiditate incidunt voluptatem natus repellat, illo accusamus? Odio dolor ipsam in ea eveniet unde voluptatibus, officia libero.',
+        "title": 'Взгляни на медного змея',
+    },
+    {
+        "audio": '/audio/001.mp3',
+        "author": 'Дмитрий Чубукин',
+        "bible": 'Бытие 22',
+        "bibleText": '',
+        "date": 'Самара, 2020',
+        "id": 3,
+        "link": 'https://www.youtube.com/@alprokope',
+        "number": '003',
+        "text": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolorem. Ad temporibus quis unde blanditiis expedita soluta, accusamus tempore ullam dolores! Nisi minima soluta odit ipsam ad repudiandae consectetur deserunt quas sit, nihil saepe corporis esse? Quod perspiciatis nesciunt doloremque, quaerat excepturi provident dignissimos sed? Provident voluptates est blanditiis vel, id a quaerat, odio ipsam dolores, doloribus minus labore. Voluptatem corrupti culpa perferendis amet sint placeat mollitia quos, magnam iste, ratione minima commodi quis? Consequatur dolor, minus id quos eum voluptas corrupti pariatur optio incidunt asperiores quam rerum consequuntur nam quis accusantium non ex ipsum a molestiae labore necessitatibus omnis cupiditate. Esse sapiente provident quo necessitatibus placeat, nesciunt accusantium quaerat aperiam vero hic expedita debitis voluptas illo pariatur vitae. Molestiae, aperiam voluptates ullam culpa odio veritatis eligendi ea vitae repellendus dolorum quis alias, dolores rerum earum ipsum optio voluptatem est nulla nemo voluptas reprehenderit? Pariatur laboriosam tempore rem magni doloribus repellendus nisi quam animi quasi! Dignissimos ab quia facilis, nisi maiores ad quasi obcaecati laboriosam cupiditate architecto, consequatur rem fuga incidunt adipisci provident, fugiat repellat dolore! Repellendus numquam, nihil veniam optio accusamus excepturi ipsum omnis natus doloremque nobis eius, expedita ipsa provident laborum. Itaque sint maxime nostrum sed eius fuga officiis, facere reprehenderit repellendus adipisci, excepturi quasi qui corporis labore nulla odio vitae porro. Officiis qui nisi quia fugit suscipit quibusdam expedita dolor ut. Inventore in, quisquam aliquam culpa perferendis ipsam, doloribus praesentium aperiam voluptatibus corporis natus. Error velit sapiente eum? Modi qui facere officiis velit quaerat amet sed nam voluptas, laborum tempora aliquid dolorem numquam nobis provident quidem culpa voluptates neque porro quae! Repellat odit doloremque, rem sint suscipit neque saepe doloribus assumenda sed natus. Sint quasi excepturi commodi placeat delectus omnis cupiditate incidunt voluptatem natus repellat, illo accusamus? Odio dolor ipsam in ea eveniet unde voluptatibus, officia libero.',
+        "title": 'Бог усмотрит Себе Агнца',
+    },
+    {
+        "audio": '/audio/001.mp3',
+        "author": 'Дмитрий Чубукин',
+        "bible": 'Псалом 89',
+        "bibleText": '',
+        "date": 'Самара, 2020',
+        "id": 4,
+        "link": 'https://www.youtube.com/@alprokope',
+        "number": '004',
+        "text": 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolorem. Ad temporibus quis unde blanditiis expedita soluta, accusamus tempore ullam dolores! Nisi minima soluta odit ipsam ad repudiandae consectetur deserunt quas sit, nihil saepe corporis esse? Quod perspiciatis nesciunt doloremque, quaerat excepturi provident dignissimos sed? Provident voluptates est blanditiis vel, id a quaerat, odio ipsam dolores, doloribus minus labore. Voluptatem corrupti culpa perferendis amet sint placeat mollitia quos, magnam iste, ratione minima commodi quis? Consequatur dolor, minus id quos eum voluptas corrupti pariatur optio incidunt asperiores quam rerum consequuntur nam quis accusantium non ex ipsum a molestiae labore necessitatibus omnis cupiditate. Esse sapiente provident quo necessitatibus placeat, nesciunt accusantium quaerat aperiam vero hic expedita debitis voluptas illo pariatur vitae. Molestiae, aperiam voluptates ullam culpa odio veritatis eligendi ea vitae repellendus dolorum quis alias, dolores rerum earum ipsum optio voluptatem est nulla nemo voluptas reprehenderit? Pariatur laboriosam tempore rem magni doloribus repellendus nisi quam animi quasi! Dignissimos ab quia facilis, nisi maiores ad quasi obcaecati laboriosam cupiditate architecto, consequatur rem fuga incidunt adipisci provident, fugiat repellat dolore! Repellendus numquam, nihil veniam optio accusamus excepturi ipsum omnis natus doloremque nobis eius, expedita ipsa provident laborum. Itaque sint maxime nostrum sed eius fuga officiis, facere reprehenderit repellendus adipisci, excepturi quasi qui corporis labore nulla odio vitae porro. Officiis qui nisi quia fugit suscipit quibusdam expedita dolor ut. Inventore in, quisquam aliquam culpa perferendis ipsam, doloribus praesentium aperiam voluptatibus corporis natus. Error velit sapiente eum? Modi qui facere officiis velit quaerat amet sed nam voluptas, laborum tempora aliquid dolorem numquam nobis provident quidem culpa voluptates neque porro quae! Repellat odit doloremque, rem sint suscipit neque saepe doloribus assumenda sed natus. Sint quasi excepturi commodi placeat delectus omnis cupiditate incidunt voluptatem natus repellat, illo accusamus? Odio dolor ipsam in ea eveniet unde voluptatibus, officia libero.',
+        "title": 'Что сделать мне доброго, чтобы иметь жизнь вечную?',
+    },
+
+]
 
 const body = document.getElementById('body')
 const dialog = document.getElementById('dialog')
-const openBtn = document.getElementById('open')
 const closeBtn = document.getElementById('close')
 
 const audio = document.getElementById('audio')
@@ -13,24 +74,36 @@ const text = document.getElementById('text')
 const title = document.getElementById('title')
 const number = document.getElementById('number')
 
+const list = document.querySelector('.main__list')
+const links = []
 
-openBtn.onclick = () => {
-    audio.setAttribute('src', tract001.audio || '')
-    author.textContent = tract001.author || ''
-    author.setAttribute('href', tract001.link || '*')
-    bible.textContent = tract001.bible || ''
-    date.textContent = tract001.date || ''
-    title.textContent = tract001.title || ''
-    number.textContent = tract001.number || ''
-    text.textContent = tract001.text || ''
+tracts.forEach( (tract, index) => {
+        const link = `<li><a href="" class="main__link"></a>${tracts[index].title}</li>`
+        links.push(link)
+    }
+)
 
+list.innerHTML = links.join('')
+
+list.onclick = (e) => {
+    const index = [...e.target.parentNode.children].indexOf(e.target)
+    const tract = tracts[index] || ''
+
+    if (tract.audio) audio.setAttribute('src', tract.audio || '')
+    author.textContent = tract.author || ''
+    author.setAttribute('href', tract.link || '*')
+    bible.textContent = tract.bible || ''
+    date.textContent = tract.date || ''
+    title.textContent = tract.title || ''
+    if (tract.number) number.textContent = `#${tract.number}` || ''
+    text.textContent = tract.text || ''
+    
     body.classList.toggle('._locked')
     dialog.showModal()
 }
+
 
 closeBtn.onclick = () => {
     dialog.close()
     body.classList.toggle('._locked')
 }
-
-console.log(tract001);
